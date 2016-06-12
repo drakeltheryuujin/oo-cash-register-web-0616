49985d3df42887033e37a54ef2e4c22d73d6f739
Kellye Greene <zephyr-eng+kellye@gust.com>
2016-06-12 18:22:41 -0400

    lucky lol

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total, :discount, :items
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    quantity.times do
+      @items << title
+    end
+    @previous_total = price
+  end
+
+  def apply_discount
+    if @discount != 0
+      @total = @total - (@total * @discount/100)
+      return "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total = @total - @previous_total
+  end
+end
